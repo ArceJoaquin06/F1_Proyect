2b443382e70b4d654dfe0ff703f5e443d18f8b73
unknown <d47797531@alumnos.itr.edu.ar>
2023-09-10 21:37:05 -0300

    me estoy durmiendo y estoy viendo videos de youtube - mercedes-amg

--- my-app/src/teams/Mercedes.js
+++ my-app/src/teams/Mercedes.js
@@ -1,13 +1,57 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './teams-css/Mercedes.css';
 
 function Mercedes() {
+
+    const Botonregresar7 = {
+        color: 'white',
+        textDecoration: 'none',
+        fontWeight: 'bold',
+      };
+  
+      
     return (
         <>
+          <div className="fondo7">
             <div>
-                <h1>Mercedes</h1>
-                <label><Link to="/team">Regresar a Escuderias</Link></label>
+                <h1 className="titulo7">Mercedes</h1>
+                <h3 className="texto7">Que no se puede decir de mercedes, ha dominado practicamente todos los años de los 2010 en adelante, con algunas excepciones, 
+                obteniendo 7 campeonatos de pilotos (1 de Nico Rosberg y 6 de Lewis Hamilton) desde 2014 hasta 2020.
+                </h3>
+                <h1 className="subtitulo7">Monoplaza</h1>
+                <h3 className="texto7">El monoplaza Actual de esta escuderia es el W14</h3>
+                <div className="imagen-centar7">
+                    <img className="imagen-W14" src="https://soymotor.com/sites/default/files/styles/team_desk/public/2023-02/mercedes-w14-f1-2023-soymotor.png?h=31f20df5&itok=ZbfSsEJL" alt=""></img>
+                </div>
+                <h3 className="subtitulo6"> Los pilotos actules son:</h3>
+                <div>
+                    <img className="imagen-ham" src="https://a.espncdn.com/combiner/i?img=/i/headshots/rpm/players/full/868.png&w=350&h=254" alt=""></img>
+                    <img className="imagen-rus" src="https://a.espncdn.com/combiner/i?img=/i/headshots/rpm/players/full/5503.png&w=350&h=254" alt=""></img>
+                </div>
+                <div>
+                    <div>
+                        <h4 className="titulo-ham">Lewis Hamilton</h4>
+                        <h4 className="titulo-rus">George Russell</h4>
+                    </div>
+                    <h4 className="texto-ham">Podios: 195</h4>
+                    <h4 className="texto-rus">Podios: 10</h4>
+               
+                    <h4 className="texto-ham">Debut: 2007</h4>
+                    <h4 className="texto-rus">Debut: 2019</h4>
+                
+                    <h4 className="texto-ham">Carreras: 324</h4>
+                    <h4 className="texto-rus">Carreras: 96</h4>
+                
+                    <h4 className="texto-ham">Mejor puesto: 1°</h4>
+                    <h4 className="texto-rus">Mejor puesto: 1°</h4>
+                </div>
+
+                <br></br>
+                <br></br>
+                <label><Link style={Botonregresar7} to="/team">Regresar a Escuderias</Link></label>
             </div>
+        </div>
         </>
     );
 }
