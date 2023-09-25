09881b7e8df0709f1f08c41c4fecd8a746aaa4f5
Joaquin Arce <d47174994@instituto.itr.edu.ar>
2023-09-25 16:58:31 -0300

    coloco las imagenes y cambio los nombres

--- my-app/src/Circuito.js
+++ my-app/src/Circuito.js
@@ -6,124 +6,31 @@ import './css/Circuito.css';
 import { Tooltip as ReactTooltip } from "react-tooltip";
 
 function Circuito() {
-    const carreras = [
-     {
-      circuitId: url("https://a.espncdn.com/i/venues/f1/circuit-info/243.svg"),
-      circuitName: {circuitName},
-      circuitNumber: 1,
-     },
-     {
-      circuitId: url("https://a.espncdn.com/i/venues/f1/circuit-info/7104.svg"),
-      circuitName: {circuitName},
-      circuitNumber: 2,
-     },
-     {
-      circuitId: url("https://a.espncdn.com/i/venues/f1/circuit-info/241.svg"),
-      circuitName: {circuitName},
-      circuitNumber: 3,
-     },
-     {
-      circuitId: url("https://a.espncdn.com/i/venues/f1/circuit-info/402.svg"),
-      circuitName: {circuitName},
-      circuitNumber: 4,
-     },
-     {
-      circuitId: url("https://a.espncdn.com/i/venues/f1/circuit-info/7105.svg"),
-      circuitName: {circuitName},
-      circuitNumber: 5,
-     },
-     {
-      circuitId: url("https://a.espncdn.com/i/venues/f1/circuit-info/255.svg"),
-      circuitName: {circuitName},
-      circuitNumber: 6,
-     },
-     {
-      circuitId: url("https://a.espncdn.com/i/venues/f1/circuit-info/238.svg"),
-      circuitName: {circuitName},
-      circuitNumber: 7,
-     },
-     {
-      circuitId: url("https://a.espncdn.com/i/venues/f1/circuit-info/51.svg"),
-      circuitName: {circuitName},
-      circuitNumber: 8,
-     },     
-     {
-      circuitId: url("https://a.espncdn.com/i/venues/f1/circuit-info/275.svg"),
-      circuitName: {circuitName},
-      circuitNumber: 9,
-     },
-     {
-      circuitId: url("https://a.espncdn.com/i/venues/f1/circuit-info/254.svg"),
-      circuitName: {circuitName},
-      circuitNumber: 10,
-     },
-     {
-      circuitId: url("https://a.espncdn.com/i/venues/f1/circuit-info/234.svg"),
-      circuitName: {circuitName},
-      circuitNumber: 11,
-     },
-     {
-      circuitId: url("https://a.espncdn.com/i/venues/f1/circuit-info/257.svg"),
-      circuitName: {circuitName},
-      circuitNumber: 12,
-     },
-     {
-      circuitId: url("https://a.espncdn.com/i/venues/f1/circuit-info/404.svg"),
-      circuitName: {circuitName},
-      circuitNumber: 13,
-     },
-     {
-      circuitId: url("https://a.espncdn.com/i/venues/f1/circuit-info/259.svg"),
-      circuitName: {circuitName},
-      circuitNumber: 14,
-     },
-     {
-      circuitId: url("https://a.espncdn.com/i/venues/f1/circuit-info/247.svg"),
-      circuitName: {circuitName},
-      circuitNumber: 15,
-     },
-     {
-      circuitId: url("https://a.espncdn.com/i/venues/f1/circuit-info/408.svg"),
-      circuitName: {circuitName},
-      circuitNumber: 16,
-     },
-     {
-      circuitId: url("https://a.espncdn.com/i/venues/f1/circuit-info/253.svg"),
-      circuitName: {circuitName},
-      circuitNumber: 17,
-     },
-     {
-      circuitId: url("https://a.espncdn.com/i/venues/f1/circuit-info/50.svg"),
-      circuitName: {circuitName},
-      circuitNumber: 18,
-     },
-     {
-      circuitId: url("https://a.espncdn.com/i/venues/f1/circuit-info/278.svg"),
-      circuitName: {circuitName},
-      circuitNumber: 19,
-     },
-     {
-      circuitId: url("https://a.espncdn.com/i/venues/f1/circuit-info/7109.svg"),
-      circuitName: {circuitName},
-      circuitNumber: 20,
-     },
-     {
-      circuitId: url("https://a.espncdn.com/i/venues/f1/circuit-info/248.svg"),
-      circuitName: {circuitName},
-      circuitNumber: 21,
-     },
-     {
-      circuitId: url("https://a.espncdn.com/i/venues/f1/circuit-info/293.svg"),
-      circuitName: {circuitName},
-      circuitNumber: 22,
-     },
-     {
-      circuitId: url("https://a.espncdn.com/i/venues/f1/circuit-info/293.svg"),
-      circuitName: {circuitName},
-      circuitNumber: 23,
-     },
-    ]
-   
+    const carreras = {
+      albert_park: "https://a.espncdn.com/i/venues/f1/circuit-info/243.svg",
+      americas: "https://a.espncdn.com/i/venues/f1/circuit-info/7104.svg",
+      bahrain: "https://a.espncdn.com/i/venues/f1/circuit-info/241.svg",
+      baku: "https://a.espncdn.com/i/venues/f1/circuit-info/402.svg",
+      catalunya: "https://a.espncdn.com/i/venues/f1/circuit-info/7105.svg",
+      hungaroring: "https://a.espncdn.com/i/venues/f1/circuit-info/255.svg",
+      interlagos: "https://a.espncdn.com/i/venues/f1/circuit-info/238.svg",
+      jeddah: "https://a.espncdn.com/i/venues/f1/circuit-info/51.svg",
+      losail: "https://a.espncdn.com/i/venues/f1/circuit-info/275.svg",
+      marina_bay: "https://a.espncdn.com/i/venues/f1/circuit-info/254.svg",
+      miami: "https://a.espncdn.com/i/venues/f1/circuit-info/234.svg",
+      monaco: "https://a.espncdn.com/i/venues/f1/circuit-info/257.svg",
+      monza: "https://a.espncdn.com/i/venues/f1/circuit-info/404.svg",
+      red_bull_ring: "https://a.espncdn.com/i/venues/f1/circuit-info/259.svg",
+      rodriguez: "https://a.espncdn.com/i/venues/f1/circuit-info/247.svg",
+      silverstone: "https://a.espncdn.com/i/venues/f1/circuit-info/408.svg",
+      spa: "https://a.espncdn.com/i/venues/f1/circuit-info/253.svg",
+      suzuka: "https://a.espncdn.com/i/venues/f1/circuit-info/50.svg",
+      vegas: "https://a.espncdn.com/i/venues/f1/circuit-info/278.svg",
+      villeneuve: "https://a.espncdn.com/i/venues/f1/circuit-info/7109.svg",
+      yas_marina: "https://a.espncdn.com/i/venues/f1/circuit-info/248.svg",
+      zandvoort: "https://a.espncdn.com/i/venues/f1/circuit-info/293.svg"
+    }
+
     const [circuitList, SetCircuitList] = useState([]);
     const [content, setcontent] = useState([]);
     const [imagenes, setimagenes] = useState([]);
@@ -143,7 +50,7 @@ function Circuito() {
     
     return (
       <>
-        <ComposableMap>
+        <ComposableMap className="container1">
         
           <Geographies geography={geoUrl}>
             {({ geographies }) =>
@@ -173,25 +80,22 @@ function Circuito() {
                     }}
              >
                 <circle  
-
-                r={5}
-                fill="#F00" 
-                stroke="#fff" 
-                strokeWidth={1} 
+                  r={5}
+                  fill="#F00" 
+                  stroke="#fff" 
+                  strokeWidth={1} 
                 />
-                
-                
-
             </Marker>
-            
           ))}
         </ComposableMap>
         <ReactTooltip
-        id="circuit"
-        content={content.carrears.circuitName}
-        
-        place="bottom"
-      />
+          id="circuit"
+          place="bottom"
+        >
+          {content && (
+            <img src={carreras[content.circuitId]} alt={`Circuito ${content.circuitName}`} />
+          )}
+        </ReactTooltip>
       </>
       )
     }
