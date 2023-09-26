86c385f75a22b2083628b2e24b7ff87a3dcc5309
Joaquin Arce <d47174994@instituto.itr.edu.ar>
2023-09-26 16:06:27 -0300

    acomodo los circuitos

--- my-app/src/Circuito.js
+++ my-app/src/Circuito.js
@@ -7,28 +7,28 @@ import { Tooltip as ReactTooltip } from "react-tooltip";
 
 function Circuito() {
     const carreras = {
-      albert_park: "https://a.espncdn.com/i/venues/f1/circuit-info/243.svg",
-      americas: "https://a.espncdn.com/i/venues/f1/circuit-info/7104.svg",
-      bahrain: "https://a.espncdn.com/i/venues/f1/circuit-info/241.svg",
+      albert_park: "https://a.espncdn.com/i/venues/f1/circuit-info/241.svg",
+      americas: "https://a.espncdn.com/i/venues/f1/circuit-info/253.svg",
+      bahrain: "https://a.espncdn.com/i/venues/f1/circuit-info/243.svg",
       baku: "https://a.espncdn.com/i/venues/f1/circuit-info/402.svg",
-      catalunya: "https://a.espncdn.com/i/venues/f1/circuit-info/7105.svg",
-      hungaroring: "https://a.espncdn.com/i/venues/f1/circuit-info/255.svg",
-      interlagos: "https://a.espncdn.com/i/venues/f1/circuit-info/238.svg",
-      jeddah: "https://a.espncdn.com/i/venues/f1/circuit-info/51.svg",
-      losail: "https://a.espncdn.com/i/venues/f1/circuit-info/275.svg",
-      marina_bay: "https://a.espncdn.com/i/venues/f1/circuit-info/254.svg",
-      miami: "https://a.espncdn.com/i/venues/f1/circuit-info/234.svg",
-      monaco: "https://a.espncdn.com/i/venues/f1/circuit-info/257.svg",
-      monza: "https://a.espncdn.com/i/venues/f1/circuit-info/404.svg",
-      red_bull_ring: "https://a.espncdn.com/i/venues/f1/circuit-info/259.svg",
-      rodriguez: "https://a.espncdn.com/i/venues/f1/circuit-info/247.svg",
-      silverstone: "https://a.espncdn.com/i/venues/f1/circuit-info/408.svg",
-      spa: "https://a.espncdn.com/i/venues/f1/circuit-info/253.svg",
-      suzuka: "https://a.espncdn.com/i/venues/f1/circuit-info/50.svg",
-      vegas: "https://a.espncdn.com/i/venues/f1/circuit-info/278.svg",
-      villeneuve: "https://a.espncdn.com/i/venues/f1/circuit-info/7109.svg",
+      catalunya: "https://a.espncdn.com/i/venues/f1/circuit-info/238.svg",
+      hungaroring: "https://a.espncdn.com/i/venues/f1/circuit-info/234.svg",
+      interlagos: "https://a.espncdn.com/i/venues/f1/circuit-info/278.svg",
+      jeddah: "https://a.espncdn.com/i/venues/f1/circuit-info/7104.svg",
+      losail: "https://a.espncdn.com/i/venues/f1/circuit-info/408.svg",
+      marina_bay: "https://a.espncdn.com/i/venues/f1/circuit-info/247.svg",
+      miami: "https://a.espncdn.com/i/venues/f1/circuit-info/7105.svg",
+      monaco: "https://a.espncdn.com/i/venues/f1/circuit-info/255.svg",
+      monza: "https://a.espncdn.com/i/venues/f1/circuit-info/259.svg",
+      red_bull_ring: "https://a.espncdn.com/i/venues/f1/circuit-info/275.svg",
+      rodriguez: "https://a.espncdn.com/i/venues/f1/circuit-info/50.svg",
+      silverstone: "https://a.espncdn.com/i/venues/f1/circuit-info/254.svg",
+      spa: "https://a.espncdn.com/i/venues/f1/circuit-info/257.svg",
+      suzuka: "https://a.espncdn.com/i/venues/f1/circuit-info/235.svg",
+      vegas: "https://a.espncdn.com/i/venues/f1/circuit-info/7109.svg",
+      villeneuve: "https://a.espncdn.com/i/venues/f1/circuit-info/51.svg",
       yas_marina: "https://a.espncdn.com/i/venues/f1/circuit-info/248.svg",
-      zandvoort: "https://a.espncdn.com/i/venues/f1/circuit-info/293.svg"
+      zandvoort: "https://a.espncdn.com/i/venues/f1/circuit-info/404.svg"
     }
 
     const [circuitList, SetCircuitList] = useState([]);
@@ -79,7 +79,7 @@ function Circuito() {
                          },
                     }}
              >
-                <circle  
+                <circle
                   r={5}
                   fill="#F00" 
                   stroke="#fff" 
