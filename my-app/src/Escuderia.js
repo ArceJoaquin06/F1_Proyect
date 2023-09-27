7c2616843c099d0ad973277dc803e45354d97c54
Joaquin Arce <d47174994@instituto.itr.edu.ar>
2023-09-27 14:01:10 -0300

    el arhex

--- my-app/src/Escuderia.js
+++ my-app/src/Escuderia.js
@@ -3,10 +3,9 @@ import { Link } from "react-router-dom";
 import './css/Escuderia.css';
 
 const botonFerrari = {
-    width: '272px',
+    width: '278px',
     height: '286px', 
     border: 'none',
-    borderRadius: '3px',
     cursor: 'pointer',
     background: 'url("https://i.pinimg.com/564x/71/79/1f/71791fa9ef030a75e32393c159c31d27.jpg") center/cover',
     color: 'transparent',
@@ -14,90 +13,81 @@ const botonFerrari = {
   }
 
   const botonWilliams = {
-    width: '272px',
+    width: '278px',
     height: '286px',
     border: 'none',
-    borderRadius: '3px',
     cursor: 'pointer',
     background: 'url("https://i.pinimg.com/564x/d9/b8/ab/d9b8abe81f772d9c6bf6b62c3e899e8f.jpg") center/cover',
     color: 'transparent',
   }
 
   const botonMcLaren = {
-    width: '272px',
+    width: '264px',
     height: '286px',
     border: 'none',
-    borderRadius: '3px',
     cursor: 'pointer',
-    background: 'url("https://i.pinimg.com/originals/2d/62/ea/2d62ea3c23f6c6123863de980e958b1f.jpg") center/cover',
+    background: 'url("https://i.pinimg.com/564x/2d/62/ea/2d62ea3c23f6c6123863de980e958b1f.jpg") center/cover',
     color: 'transparent',
   }
 
   const botonHaas = {
-    width: '272px',
+    width: '278px',
     height: '286px',
     border: 'none',
-    borderRadius: '3px',
     cursor: 'pointer',
-    background: 'url("https://i.pinimg.com/originals/c8/e0/aa/c8e0aa63bbd7ddac8774a1fde44a357a.jpg") center/cover',
+    background: 'url("https://i.pinimg.com/564x/d8/23/ff/d823ff42ba8007e37fecef224c611653.jpg") center/cover',
     color: 'transparent',
   }
 
   const botonAston = {
-    width: '272px',
+    width: '278px',
     height: '286px',
     border: 'none',
-    borderRadius: '3px',
     cursor: 'pointer',
-    background: 'url("https://soymotor.com/sites/default/files/styles/top_3_263x263/public/imagenes/equipo/logo-aston-martin-f1-2021.png.webp?itok=eEc_TuAj") center/cover',
+    background: 'url("https://i.pinimg.com/564x/01/17/2b/01172b8d7fb181ec0593d020fe621c3c.jpg") center/cover',
     color: 'transparent',
   }
 
   const botonAlfaRomeo = {
-    width: '272px',
+    width: '278px',
     height: '286px',
     border: 'none',
-    borderRadius: '3px',
     cursor: 'pointer',
-    background: 'url("https://as01.epimg.net/motor/imagenes/2019/02/01/formula_1/1549017855_549790_1549018043_noticia_normal.jpg") center/cover',
+    background: 'url("https://i.pinimg.com/564x/9c/6a/56/9c6a56c5c1992847b9d863780bc82019.jpg") center/cover',
     color: 'transparent',
   }
 
   const botonRedBull = {
-    width: '272px',
+    width: '278px',
     height: '286px',
     border: 'none',
-    borderRadius: '3px',
     cursor: 'pointer',
-    background: 'url("https://cdn-2.motorsport.com/images/mgl/Y99JQRbY/s8/red-bull-racing-logo-1.jpg") center/cover',
+    background: 'url("https://i.pinimg.com/564x/42/b7/0d/42b70df0294860a892c33975d6297676.jpg") center/cover',
     color: 'transparent',
   }
 
   const botonAlpine = {
-    width: '272px',
+    width: '278px',
     height: '286px',
     border: 'none',
-    borderRadius: '3px',
     cursor: 'pointer',
-    background: 'url("https://hips.hearstapps.com/hmg-prod/images/et3dwsmxuam3is8-1612973513.jpg") center/cover',
+    background: 'url("https://i.pinimg.com/564x/26/16/ee/2616ee30a26e380b74620a9713edc72c.jpg") center/cover',
     color: 'transparent',
   }
 
   const botonMercedes = {
-    width: '272px',
+    width: '278px',
     height: '286px',
     border: 'none',
-    borderRadius: '3px',
     cursor: 'pointer',
     background: 'url("https://w0.peakpx.com/wallpaper/952/703/HD-wallpaper-amg-petronas-f1-formula-1-formule-1-mercedes.jpg") center/cover',
     color: 'transparent',
   }
 
   const botonAlphaTauri = {
-    width: '270px',
+    width: '278px',
     height: '286px',
     border: 'none',
-    borderRadius: '3px',
     cursor: 'pointer',
     background: 'url("https://i.pinimg.com/564x/48/76/d7/4876d7095564767c4ed19bb6405b811f.jpg") center/cover',
     color: 'transparent',
